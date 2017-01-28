89f917454641e2e725d83ad7ee19773996dac9fb
NolanMullins <nolan.mullins@me.com>
2017-01-28 13:24:30 -0500

    basic functionality of calling cmds working

--- a1/src/fnc.c
+++ a1/src/fnc.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 #include "list.h"
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
+void clr(void* data)
+{
+	free((char*)data);
+}
+
+char** conv(List* cmd)
+{
+	char** arr = malloc(sizeof(char*)*(listSize(cmd)+1));
+	int a = 0;
+	while (listSize(cmd) > 0)
+		arr[a++] = (char*)listRemove(cmd, 0);
+	return arr;
+}
+
 void child(List* cmd)
 {
-	printf("%s\n", (char*)listGet(cmd,0));
+	//int size = listSize(cmd);
+	char** args = conv(cmd);
+
+	execvp(args[0], args);
+
+	free(args);
 	exit(0);
 }
 
 void runCmd(List* cmd)
 {
+	if (strcmp("exit", (char*)listGet(cmd, 0)) == 0)
+		exit(0);
+	//fork to run process on
 	pid_t id = fork();
 	if (id == 0)
 		child(cmd);
--- a1/src/main.c
+++ a1/src/main.c
@@ -15,15 +15,11 @@ int main()
 	char line[513];
 	while (1)
 	{
-		printf("$ ");
+		printf("> ");
 		line[0] = '\0';
 		fgets(line, 512, stdin);
 		List* myList = parseLine(line);
-		int tmp = strcmp("exit", (char*)listGet(myList, 0));
-		if (tmp != 0)
-			runCmd(myList);
-		listClear(myList, freeData);
-		if (tmp == 0)
-			exit(0);
+		runCmd(myList);
+		myList = listClear(myList, freeData);
 	}
 }
